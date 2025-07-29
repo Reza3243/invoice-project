01594fb5be1257041edf6daa3175b0a66fe79bf1
Reza <etemad3243@gmaile.com>
2025-07-29 09:04:54 +0200

    Add item input and total price calculation

--- invoice.py
+++ invoice.py
@@ -1,3 +1,12 @@
 print("Welcome to Invoice App!")
 customer = input("Enter customer name: ")
+
+item = input("Enter item name: ")
+quantity = int(input("Enter quantity: "))
+price = float(input("Enter price per item: "))
+total = quantity * price
+
+print(f"\nInvoice for {customer}")
+print(f"{quantity} x {item} = {total} EUR")print("Welcome to Invoice App!")
+customer = input("Enter customer name: ")
 print(f"Invoice for {customer}")
