d972b8bd63a5c100906333f005807a14dc9bc319
Reza <etemad3243@gmaile.com>
2025-07-29 09:17:09 +0200

    Add saving invoice data to file

--- invoice.py
+++ invoice.py
@@ -7,6 +7,7 @@ price = float(input("Enter price per item: "))
 total = quantity * price
 
 print(f"\nInvoice for {customer}")
-print(f"{quantity} x {item} = {total} EUR")print("Welcome to Invoice App!")
-customer = input("Enter customer name: ")
-print(f"Invoice for {customer}")
+print(f"{quantity} x {item} = {total} EUR")
+
+with open("invoice.txt", "a") as file:
+    file.write(f"{customer},{item},{quantity},{price},{total}\n")
